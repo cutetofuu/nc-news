543256ace70cd4ca7c5c4501a58695cf57fb1b72
Kate Dollete <cfdollete@up.edu.ph>
2023-02-20 12:45:31 +0000

    Create files for /api/topics endpoint

---
+++ models/topics.models.js
@@ -0,0 +1,13 @@
+const db = require("../db/connection");
+
+exports.fetchTopics = () => {
+  return db
+    .query(
+      `
+    SELECT * FROM topics
+    `
+    )
+    .then(({ rows }) => {
+      return rows;
+    });
+};
