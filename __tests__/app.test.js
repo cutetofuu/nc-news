3caa796761b32aece176cb113e5deb28e883a699
Kate Dollete <cfdollete@up.edu.ph>
2023-02-20 12:27:45 +0000

    Seed dev and test databases

---
+++ __tests__/app.test.js
@@ -0,0 +1,13 @@
+const request = require("supertest");
+const app = require("../app");
+const seed = require("../db/seeds/seed");
+const testData = require("../db/data/test-data/index");
+const db = require("../db/connection");
+
+beforeEach(() => {
+  return seed(testData);
+});
+
+afterAll(() => {
+  return db.end();
+});
