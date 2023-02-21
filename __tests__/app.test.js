e83c315fc8043284f0e229d6cb795eda23933bea
Kate Dollete <cfdollete@up.edu.ph>
2023-02-21 15:31:51 +0000

    Add tests for GET users requests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -340,4 +340,32 @@ describe("articles", () => {
         });
     });
   });
+
+  describe.only("GET /api/users", () => {
+    it("200: responds with an array", () => {
+      return request(app)
+        .get("/api/users")
+        .expect(200)
+        .then(({ body }) => {
+          const { users } = body;
+          expect(users).toBeInstanceOf(Array);
+        });
+    });
+    it("200: responds with all the users with the correct keys", () => {
+      return request(app)
+        .get("/api/users")
+        .expect(200)
+        .then(({ body }) => {
+          const { users } = body;
+          expect(users).toHaveLength(4);
+          users.forEach((user) => {
+            expect(user).toMatchObject({
+              username: expect.any(String),
+              name: expect.any(String),
+              avatar_url: expect.any(String),
+            });
+          });
+        });
+    });
+  });
 });
