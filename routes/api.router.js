f78f961c692f88bee835fc3166841aa060c9eb97
Kate Dollete <cfdollete@up.edu.ph>
2023-02-23 12:18:03 +0000

    Create comments router

--- routes/api.router.js
+++ routes/api.router.js
@@ -2,6 +2,7 @@ const apiRouter = require("express").Router();
 const topicsRouter = require("./topics.router");
 const articlesRouter = require("./articles.router");
 const usersRouter = require("./users.router");
+const commentsRouter = require("./comments.router");
 const { getEndpoints } = require("../controllers/api.controller");
 
 apiRouter.get("/", getEndpoints);
@@ -11,5 +12,7 @@ apiRouter.use("/topics", topicsRouter);
 apiRouter.use("/articles", articlesRouter);
 
 apiRouter.use("/users", usersRouter);
+
+apiRouter.use("/comments", commentsRouter);
 
 module.exports = apiRouter;
