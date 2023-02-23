3debf1b111910adc3932e7b858a8bbd9c80f46ec
Kate Dollete <cfdollete@up.edu.ph>
2023-02-23 15:44:35 +0000

    Add query and tests for PATCH comment requests

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -1,10 +1,22 @@
-const { removeComment } = require("../models/comments.models");
+const { removeComment, updateComment } = require("../models/comments.models");
 
 exports.deleteComment = (req, res, next) => {
   const { comment_id } = req.params;
   removeComment(comment_id)
     .then(() => {
       res.status(204).send();
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.patchComment = (req, res, next) => {
+  const { inc_votes } = req.body;
+  const { comment_id } = req.params;
+  updateComment(inc_votes, comment_id)
+    .then((comment) => {
+      res.status(200).send({ comment });
     })
     .catch((err) => {
       next(err);
