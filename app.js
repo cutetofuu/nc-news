519fb7a2e2af8db2546fa3766d9593a25227731f
Kate Dollete <cfdollete@up.edu.ph>
2023-02-23 13:56:48 +0000

    Refactor app.js

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
 const express = require("express");
-const { getEndpoints } = require("./controllers/api.controller");
-const { getTopics } = require("./controllers/topics.controller");
-const {
-  getArticles,
-  getOneArticle,
-  getArticleComments,
-  postComment,
-  patchArticle,
-} = require("./controllers/articles.controller");
-const { getUsers } = require("./controllers/users.controller");
-const { deleteComment } = require("./controllers/comments.controller");
+const apiRouter = require("./routes/api.router");
 const {
   handle404NonExistentPaths,
   handlePSQL400s,
@@ -21,23 +11,7 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/api", getEndpoints);
-
-app.get("/api/topics", getTopics);
-
-app.get("/api/articles", getArticles);
-
-app.get("/api/articles/:article_id", getOneArticle);
-
-app.get("/api/articles/:article_id/comments", getArticleComments);
-
-app.post("/api/articles/:article_id/comments", postComment);
-
-app.patch("/api/articles/:article_id", patchArticle);
-
-app.get("/api/users", getUsers);
-
-app.delete("/api/comments/:comment_id", deleteComment);
+app.use("/api", apiRouter);
 
 app.use(handle404NonExistentPaths);
 
