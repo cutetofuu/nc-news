5019efb6ce694fc39ee532b1c38495817d5ccdba
Kate Dollete <cfdollete@up.edu.ph>
2023-03-13 15:32:01 +0000

    Refactor CORS code in app.js

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ const {
 } = require("./controllers/error-handling.controller");
 const cors = require("cors");
 
-app.use(cors());
-
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
